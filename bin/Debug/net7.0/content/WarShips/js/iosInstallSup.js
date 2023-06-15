e991cad2d514d2abd44d11aff80b043ca25aa63b
kirill <kinikitin2@gmail.com>
2023-06-15 16:12:35 +0900

    =test

--- bin/Debug/net7.0/content/WarShips/js/iosInstallSup.js
+++ bin/Debug/net7.0/content/WarShips/js/iosInstallSup.js
@@ -1,12 +1,17 @@
 
-const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
-}
+// const isIos = function () {
+//     window.device
+//     const userAgent = window.navigator.userAgent.toLowerCase();
+//     const isIos = /(iphone|ipad|ipod|applewebkit)/.test(userAgent);
+//     return isIos;
+// }
 
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+// const isInStandaloneMode = function () {
+//     const isStandalone = ('standalone' in window.navigator) && (window.navigator.standalone);
+//     return isStandalone;
+// }
 
 
-if (isIos() && !isInStandaloneMode()) {
-    this.setState({ showInstallMessage: true });
-}
+// if (isIos() && !isInStandaloneMode()) {
+//     this.setState({ showInstallMessage: true });
+// }
